41afc170df6a82af2242bfedef5c5a48763744ae
Myssal <143514975+myssal@users.noreply.github.com>
2025-09-12 21:45:28 +0700

    feat: l2d metadata view

---
+++ DianaLab.GUI/Views/L2DMetadataEditorWindow.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace DianaLab.GUI.Views
+{
+    /// <summary>
+    /// Interaction logic for L2DMetadataEditorWindow.xaml
+    /// </summary>
+    public partial class L2DMetadataEditorWindow : Window
+    {
+        public L2DMetadataEditorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DianaLab.GUI/Views/Live2DAssetsView.xaml.cs
+++ DianaLab.GUI/Views/Live2DAssetsView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using System.Windows;
 
 namespace DianaLab.GUI.Views
 {
@@ -7,6 +8,12 @@ namespace DianaLab.GUI.Views
         public Live2DAssetsView()
         {
             InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            L2DMetadataEditorWindow editorWindow = new L2DMetadataEditorWindow();
+            editorWindow.ShowDialog(); // Use ShowDialog to make it a modal window
         }
     }
 }
